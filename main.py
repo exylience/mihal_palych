a8ec40e753bb6bed29106412bee985b3de575ee7
Yaroslav Murashkin <exylience.m@gmail.com>
2021-12-07 01:46:24 +0700

    Оптимизация под личку бота

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import json
 
 from vk_api import longpoll
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
-from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.longpoll import VkLongPoll, VkEventType
 
 # API token
 TOKEN = "7acbc17a73baad4d0285bf07f8c3202d15745100da5077f64cafadf4ee858f3e83ff289f77f6cb3c1a61c"
@@ -16,13 +16,13 @@ vk_session = vk_api.VkApi(token=TOKEN)
 vk = vk_session.get_api()
 
 # сетевой демон для работы с сообщениями
-longpoll = VkBotLongPoll(vk_session, 170215431)
+longpoll = VkLongPoll(vk_session)
 
 
 upload = vk_api.VkUpload(vk)
 
 keyboard = VkKeyboard(one_time = False)
-keyboard.add_button("лессс гоу", color = VkKeyboardColor.POSITIVE)
+keyboard.add_button("драйв", color = VkKeyboardColor.POSITIVE)
 
 
 with open("assets/allowed_pick.json", "r", encoding="utf-8") as read_file:
@@ -34,6 +34,17 @@ with open("assets/banned_pick.json", "r", encoding="utf-8") as read_file:
     banned_pick = json.load(read_file)
     banned_pick = banned_pick.get("data")
     read_file.close()
+
+def test_reply(event):
+    if event.to_me:
+        vk.messages.send(
+            message="ответ",
+            key=("9336ef10bd5469381c84f831f563f8b7fb8bd567"),
+            server=("https://lp.vk.com/wh170215431"),
+            ts=("38986"),
+            random_id=vk_api.utils.get_random_id(),
+            user_id=event.user_id
+        )
 
 def reply_to_tiktok(event):
     if event.from_chat:
@@ -55,7 +66,7 @@ def santa(event):
             server=("https://lp.vk.com/wh170215431"),
             ts=("38986"),
             random_id=vk_api.utils.get_random_id(),
-            chat_id=event.chat_id
+            user_id=event.user_id
         )
     else:
         random_person_index = randint(0, len(allowed_pick) - 1)
@@ -79,8 +90,6 @@ def santa(event):
             json.dump(dict, read_file, ensure_ascii=False)
             read_file.close()
 
-        print(allowed_pick)
-
         banned_pick.append(event.object.from_id)
 
         with open("assets/banned_pick.json", "w", encoding="utf-8") as read_file:
@@ -88,7 +97,7 @@ def santa(event):
             json.dump(dict, read_file, ensure_ascii=False)
             read_file.close()
 
-        if event.from_chat:
+        if event.to_me:
             vk.messages.send(
                 message=f"@id{random_person_id} ({random_person_name})",
                 attachment=attachment,
@@ -96,21 +105,24 @@ def santa(event):
                 server=("https://lp.vk.com/wh170215431"),
                 ts=("38986"),
                 random_id=vk_api.utils.get_random_id(),
-                chat_id=event.chat_id
+                user_id=event.user_id
             )
     
 
 # запускаем сетевого демона
 for event in longpoll.listen():
     # если пришло сообщение
-    if event.type == VkBotEventType.MESSAGE_NEW:
-        message = str(event.object.text).split(".")
+    if event.type == VkEventType.MESSAGE_NEW:
+        message = str(event.text).split(".")
         
         if 'tiktok' in message or 'tiktok' in str(event) or 'тик ток' in str(event):
             reply_to_tiktok(event)
 
-        if "лессс гоу" in str(event):
+        if "драйв" in str(event):
             santa(event)
+
+        if "test" in str(event.text):
+            test_reply(event)
 
 
 
